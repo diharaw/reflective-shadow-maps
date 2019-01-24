898c545e06f2b8064939c1e741708d98521921f9
Diharaw <shredhead94@gmail.com>
2019-01-24 11:21:51 +0530

    Updated scene shader. Added Cornell Box mesh.

--- src/shader/scene_fs.glsl
+++ src/shader/scene_fs.glsl
@@ -5,19 +5,19 @@ in vec3 PS_IN_FragPos;
 in vec3 PS_IN_Normal;
 in vec2 PS_IN_TexCoord;
 
-uniform sampler2D s_Diffuse;
+uniform vec4 u_Diffuse;
 
 void main()
 {
 	vec3 light_pos = vec3(200.0, 200.0, 0.0);
 	vec3 light_dir = normalize(light_pos - PS_IN_FragPos);
 
-	vec4 diffuse = texture(s_Diffuse, PS_IN_TexCoord);
+    vec4 diffuse = u_Diffuse;
 
 	if (diffuse.w < 0.1)
 		discard;
 
-	vec3 color = dot(PS_IN_Normal, light_dir) * diffuse.xyz;
+	//vec3 color = dot(PS_IN_Normal, light_dir) * diffuse.xyz;
 
-	PS_OUT_Color = vec4(color, 1.0);
+	PS_OUT_Color = vec4(diffuse.xyz, 1.0);
 }
